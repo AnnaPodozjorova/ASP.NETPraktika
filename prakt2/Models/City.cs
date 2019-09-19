bdeb9ffa5ed2c38b3ae9a0c3b11316a7a41148e9
student <student@vk.edu.ee>
2019-09-19 10:42:04 +0300

    model

---
+++ prakt2/Models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prakt2.Models
+{
+    public class City
+    {
+        public long CityId { get; set; }
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string District { get; set; }
+        public long Population { get; set; }
+    }
+}
