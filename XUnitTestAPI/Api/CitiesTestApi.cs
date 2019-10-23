43cb7075a30d741e49f1ac8fbfea4a8b77a63740
Пётр Круглов <tankopi90@mail.ru>
2019-10-23 19:15:43 +0300

    Cities delete and post tests

---
+++ XUnitTestAPI/Api/CitiesTestApi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ASP.NET;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace XUnitTestAPI.Api
+{
+    public class CitiesTestApi
+    {
+        private readonly HttpClient _client;
+
+        public CitiesTestApi()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Startup>());
+        }
+
+        [Theory]
+        [InlineData("DELETE", "33")]
+        public async Task DeleteCity(string method, string id = null)
+        {
+            var request = new HttpRequestMessage(new HttpMethod(method), $"api/Cities/{id}");
+
+            var response = await _client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("POST")]
+        public async Task PostCities(string method)
+        {
+            var request = new HttpRequestMessage(new HttpMethod(method), "api/Cities/all");
+
+            var response = await _client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
